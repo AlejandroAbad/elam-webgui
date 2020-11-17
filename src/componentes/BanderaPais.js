5e7041d6cc1a66726b99e6cb016351e0f2f32ccc
Alejandro Abad <alejandro.abad@hefame.es>
2020-11-17 11:20:21 +0100

    BanderaPais usa el codigo como alt por defecto

--- src/componentes/BanderaPais.js
+++ src/componentes/BanderaPais.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const BanderaPais = ({codigoPais, nombrePais, className}) => {
-	return <img alt={nombrePais ?? ""} src={`https://www.countryflags.io/` + codigoPais + `/flat/24.png`} className={className} />
+	return <img alt={nombrePais ?? codigoPais} src={`https://www.countryflags.io/` + codigoPais + `/flat/24.png`} className={className} />
 }
 
 export default BanderaPais;
